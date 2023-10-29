abdcf269352abe169781a7742b594fbfdf61a285
Shimey Loo <shimeyloo@gmail.com>
2023-10-28 17:47:10 -0700

    Created adopted homepage

--- swipe4pets/src/index.js
+++ swipe4pets/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import App from "./App";
+import AdopterHomepage from "./Pages/adopter_homepage.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AdopterHomepage />
   </React.StrictMode>
 );
