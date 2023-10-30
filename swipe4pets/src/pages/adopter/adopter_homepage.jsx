2d33ff0b8b8ef63f2c4c7b7c683256fddcae3f61
Shimey Loo <shimeyloo@gmail.com>
2023-10-30 13:50:18 -0700

    fixed some merge conflicts

---
+++ swipe4pets/src/pages/adopter/adopter_homepage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Profile from '../../Components/Adopter/Profile.jsx';
+import NewSearch from '../../Components/Adopter/NewSearch.jsx';
+import Matches from '../../Components/Adopter/Matches.jsx';
+
+
+function AdopterHomepage() {
+  return (
+    <Container>
+      <h1 class="text-center">Adopter HomePage</h1>
+      <Row>
+        <Col> 
+          <Profile />
+        </Col>
+        <Col>
+          <Matches />
+          <NewSearch />
+        </Col>
+      </Row>
+    </Container>
+   
+  );
+}
+
+export default AdopterHomepage;
