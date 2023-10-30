7a9c1ea833eb3144764a2a2d4d9fbaae3b29a41a
Shimey Loo <shimeyloo@gmail.com>
2023-10-30 13:48:05 -0700

    fixed merge issues

--- swipe4pets/src/index.js
+++ swipe4pets/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App";
-import AdopterHomepage from "./Pages/adopter_homepage.jsx";
-import SwipeProfiles from "./Pages/swipe_profiles.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <AdopterHomepage /> */}
-    <SwipeProfiles />
+    <App />
   </React.StrictMode>
 );
--- swipe4pets/src/pages/adopter/AdopterHome.js
+++ swipe4pets/src/pages/adopter/AdopterHome.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const AdopterHome = () => {
-  return <h1>This is the adopter user home page or matches page. Here a user can view animals they have already matched with, manage those matches, or start a new pet search.</h1>;
+  return (
+    <h1>
+      This is the adopter user home page or matches page. Here a user can view
+      animals they have already matched with, manage those matches, or start a
+      new pet search.
+    </h1>
+  );
 };
 
 export default AdopterHome;
