9b34006c62b7cffbae82e512e0fe51bb7b4e304c
wallskel <wallskel@oregonstate.edu>
2023-11-06 15:48:18 -0600

    changes to signup pages to make it multistep, changes to App.css

--- swipe4pets/src/pages/signup/SignupPt1.js
+++ swipe4pets/src/pages/signup/SignupPt1.js
@@ -1,32 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate } from 'react-router-dom';
 
 export default function SignupPt1() {
+  const navigate = useNavigate();
+
+  let accountType = 0;
+
+  // Function sets account type as either Shelter (1) or Adopter (2)
+  const setAccountType = (n) => {
+    accountType = n;
+  }
+
+  // Function navigates to either shelter or adoption page, based on button selection
+  const nextPage = () => {
+    if (accountType == 1){
+      navigate("/sheltersignup");
+    } else if (accountType == 2) {
+      navigate("/adoptersignup");
+    } else {
+      alert("ERROR: Select account type");
+    }
+  }
+
 
   return (
-
     <div class="center">
       <h1>Sign Up</h1>
       <form>
         <div class="signup">
+          <h1> Step 1</h1>
           <label>Select One</label>
           <br></br>
-          <input type='radio' id='shelterAcct' name='accountType' value='shelterAcct'></input>
-          <label for='shelterAcct'>Shelter</label> &nbsp; &nbsp;
-          <input type='radio' id='adopterAcct' name='accountType' value='adopterAcct'></input>
-          <label for='adopterAcct'>Adopter</label>
-          <br></br>
-          <label>Email</label><br></br>
-          <input type='text' name='signup-email' required></input> <br></br>
-          <label>Password</label><br></br>
-          <input type='password' name='signup-password' required></input> <br></br> <br></br>
-          <button>Continue</button>
-
+          <button type='button' id='shelterType' onClick={() => setAccountType(1)}>Shelter</button>
           <br></br> <br></br>
-          <Link to="/sheltersignup"><button>Shelter</button></Link>
-
+          <button type='button' id='adopterType' onClick={() => setAccountType(2)}>Adopter</button>
+          <br></br><br></br>
+          <Link to="/"><button>Back</button></Link>
           &nbsp; &nbsp;
-          <Link to="/adoptersignup"><button>Adopter</button></Link>
+          <button type='button' id='nextButton' onClick={() => nextPage()}> Next </button>
         </div>
       </form>
     </div>
