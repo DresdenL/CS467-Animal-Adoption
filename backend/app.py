eca9412653d3e4fef788f7e4a6ad54af938f19c8
Kelsey Schmidt <schmkels@oregonstate.edu>
2023-11-14 20:25:49 -0600

    adjusting env

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-app.config['MYSQL_CURSORCLASS'] = os.getenv('MYSQL_CURSORCLASS')
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 # start mySQL connection
 mysql = MySQL(app)
